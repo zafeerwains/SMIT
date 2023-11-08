8f47e2cb0c5c5a0d3f9118cd5cbda88f31c4b0f7
Muhammad Zafeer <zafeerwains@gmail.com>
2023-11-08 07:22:54 +0500

    React class 2

---
+++ REACT class 2/week-2/src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <>
+            <footer>
+                <div className="container-xxl">
+                    <div className="row">
+                        <div className="col">
+                            <p> &copy; All Right REserved.</p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </>
+    )
+}
